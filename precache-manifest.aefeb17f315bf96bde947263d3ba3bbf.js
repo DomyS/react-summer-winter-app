eafba0ba26c4313700ac9828e0cc6677a4312302
domys <domante.stirbyte@gmail.com>
2019-06-20 12:28:11 +0200

    Updates

---
+++ precache-manifest.aefeb17f315bf96bde947263d3ba3bbf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21bff57e68e51c962898318121a13ce4",
+    "url": "/react-summer-winter-app/index.html"
+  },
+  {
+    "revision": "d2ff1f4bfd519f52615b",
+    "url": "/react-summer-winter-app/static/css/main.0da99f66.chunk.css"
+  },
+  {
+    "revision": "607320d00d64b68e18da",
+    "url": "/react-summer-winter-app/static/js/2.2050718e.chunk.js"
+  },
+  {
+    "revision": "d2ff1f4bfd519f52615b",
+    "url": "/react-summer-winter-app/static/js/main.fdc70869.chunk.js"
+  },
+  {
+    "revision": "2b94edb7a9f93cfc893a",
+    "url": "/react-summer-winter-app/static/js/runtime~main.ddb743dc.js"
+  }
+]);
